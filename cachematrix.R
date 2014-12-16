1d8438eaf3aa7cbbafd9572b955fe14a5249e262
marier1981 <b95236@W61936.danskenet.net>
2014-12-16 14:16:37 +0100

    My first attempt at github

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Save the inverse of a matrix, such that it may be reused without reevaluating it
 
-## Write a short comment describing this function
-
+## Create the structure of the new object, which is able to cache the inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+        ## Initially, the inverse is empty
+        m <- NULL
+        
+        ## If we wish to reset the matrix to a new one, then the inverse is deleted
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        
+        ## Retrieve the stored matrix
+        get <- function() x
+        
+        ## Assign the inverse of the matrix to 'm'
+        setInverse <- function(inverse) m <<- inverse
+        
+        ## Retrieve the value of 'm', which when set, is the inverse
+        getInverse <- function() m
+        
+        ## Make the object
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## Return the inverse; if it has not been stored, then calculate it, otherwise retrieve it from cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Retrieve what is stored as the inverse
+        m <- x$getInverse()
+        
+        ## If the inverse exists (is not null) then return and exit
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        
+        ## Otherwise read in the matrix
+        InverseMatrix <- x$get()
+        
+        ## Solve to obtain the inverse
+        m <- solve(InverseMatrix, ...)
+        
+        ## Remember to store the inverse in the cache
+        x$setInverse(m)
+        
+        ## Return the inverse
+        m
 }
